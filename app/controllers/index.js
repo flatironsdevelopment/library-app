88c4fb11c24b4e5dd7377dbb29fb48ccbf6037a0
Alberto Aragon <ronin8503@gmail.com>
2018-11-01 16:54:25 -0500

    Using promises on save function of email address

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -11,13 +11,14 @@ export default Controller.extend({
     saveInvitation() {
       const email = this.get('emailAddress');
 
-      const newInvitation = this.store.createRecord('invitation', { email: email });
-      newInvitation.save();
-      this.set(
-        'responseMessage',
-        `Thank you! We've just saved your email address: ${this.get('emailAddress')}`
-      );
-      this.set('emailAddress', '');
+      const newInvitation = this.store.createRecord('invitation', { email });
+      newInvitation.save().then(response => {
+        this.set(
+          'responseMessage',
+          `Thank you! We save your email address with the following id: ${response.get('id')}`
+        );
+        this.set('emailAddress', '');
+      });
     }
   }
 });
